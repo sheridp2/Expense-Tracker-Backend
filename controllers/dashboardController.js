fdc9e17b0a65f36cdb49fe37bb30c680a3320e69
Patrick Sheridan <sheridp2@gmail.com>
2025-08-04 09:27:13 -0700

    Moved backend to it's own repo

---
+++ controllers/dashboardController.js
@@ -0,0 +1,78 @@
+const Income = require("../models/Income");
+const Expense = require("../models/Expense");
+const { isValidObjectId, Types } = require("mongoose");
+
+exports.getDashboardData = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const userObjectId = new Types.ObjectId(String(userId));
+
+    const totalIncome = await Income.aggregate([
+      { $match: { userId: userObjectId } },
+      { $group: { _id: null, total: { $sum: "$amount" } } },
+    ]);
+
+    const totalExpense = await Expense.aggregate([
+      { $match: { userId: userObjectId } },
+      { $group: { _id: null, total: { $sum: "$amount" } } },
+    ]);
+
+    //Income tansactions in last 60 days
+    const last60DaysIncomeTransactions = await Income.find({
+      userId,
+      date: { $gte: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000) },
+    }).sort({ date: -1 });
+
+    //Last 60 day total
+    const incomeLast60Days = last60DaysIncomeTransactions.reduce(
+      (sum, transaction) => sum + transaction.amount,
+      0
+    );
+
+    //Expenses in the last 30 days
+    const last30DaysExpenseTransactions = await Expense.find({
+      userId,
+      date: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
+    }).sort({ date: -1 });
+
+    //Total expenses last 30 days
+    const expensesLast30Days = last30DaysExpenseTransactions.reduce(
+      (sum, transaction) => sum + transaction.amount,
+      0
+    );
+
+    //Last 5 transations (income + expense)
+    const last5Transactions = [
+      ...(await Income.find({ userId }).sort({ date: -1 }).limit(5)).map(
+        (txn) => ({
+          ...txn.toObject(),
+          type: "income",
+        })
+      ),
+      ...(await Expense.find({ userId }).sort({ date: -1 }).limit(5)).map(
+        (txn) => ({
+          ...txn.toObject(),
+          type: "expense",
+        })
+      ),
+    ].sort((a, b) => b.date - a.date);
+
+    res.json({
+      totalBalance:
+        (totalIncome[0]?.total || 0) - (totalExpense[0]?.total || 0),
+      totalIncome: totalIncome[0]?.total || 0,
+      totalExpenses: totalExpense[0]?.total || 0,
+      last30DaysExpenses: {
+        total: expensesLast30Days,
+        transactions: last30DaysExpenseTransactions,
+      },
+      last60DaysIncome: {
+        total: incomeLast60Days,
+        transactions: last60DaysIncomeTransactions,
+      },
+      recentTransactions: last5Transactions,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error });
+  }
+};

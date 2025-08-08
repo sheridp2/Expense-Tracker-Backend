e711e4b07ae7dca9c2008ad5b6651eeb0e4a04be
Patrick Sheridan <sheridp2@gmail.com>
2025-08-08 14:23:33 -0700

    wip

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(express.json());
 
 connectDB();
 
+app.get("/", (req, res) => res.send("Expense Tracker Backend"));
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
@@ -31,3 +33,5 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+module.exports = app;
